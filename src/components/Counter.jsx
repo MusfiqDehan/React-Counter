e0292973d98f8176b92b5f8d97f8edcf38af7d8a
Md. Musfiqur Rahaman <musfiqdehan@gmail.com>
2022-11-03 06:31:16 +0000

    add total click counts

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,21 +9,35 @@ const btnStyle = {
   border: "none",
   cursor: "pointer",
   fontSize: "30px",
-  boxShadow: "0 0 5px 5px gray"
+  boxShadow: "0 0 5px 5px gray",
 };
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [clickCount, setClickCount] = useState(0);
 
   return (
     <div>
-      <button style={btnStyle} onClick={() => setCount(count + 1)}>
+      <button
+        style={btnStyle}
+        onClick={() => {
+          setCount(count + 1);
+          setClickCount(clickCount + 1);
+        }}
+      >
         +
       </button>
 
       <h1>{count}</h1>
+      <h2>You have clicked total {clickCount} times</h2>
 
-      <button style={btnStyle} onClick={() => setCount(count - 1)}>
+      <button
+        style={btnStyle}
+        onClick={() => {
+          setCount(count + 1);
+          setClickCount(clickCount + 1);
+        }}
+      >
         -
       </button>
     </div>
